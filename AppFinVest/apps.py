b0e5fa6d4ee3212757eab13a095a6e9cd02db88f
Gustavo Resende <gustavodcr5000@gmail.com>
2025-01-11 20:57:18 -0300

    corrigido bug scheduler iniciar 2 vezes

--- AppFinVest/apps.py
+++ AppFinVest/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 class AppfinvestConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'AppFinVest'
-
-    def ready(self):
-        from .scheduler import start_scheduler
-        start_scheduler()
--- FinVest/urls.py
+++ FinVest/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from AppFinVest.scheduler import start_scheduler 
+start_scheduler()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AppFinVest.urls')),
