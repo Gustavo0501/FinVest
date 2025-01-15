a326467d7e60748de97bb9fbcfe18c25d55df360
Gustavo <tygustavo5@gmail.com>
2025-01-15 00:40:26 -0300

    tela de perfil inveestidor e perfil endividado e a logica por tras

--- AppFinVest/views/RegistroEtapa2View.py
+++ AppFinVest/views/RegistroEtapa2View.py
@@ -6,7 +6,7 @@ from AppFinVest.decorators import registro_required
 from AppFinVest.models import Usuario
 from AppFinVest.formularios import FormularioInfoFinanceiras
 from django.contrib.auth.hashers import make_password
-
+from decimal import Decimal
 
 class RegistroEtapa2View(View):
     template_name = 'AppFinVest/pages/registro_etapa2.html'
@@ -24,30 +24,37 @@ class RegistroEtapa2View(View):
             return redirect('registro')
 
         dados_pessoais = request.session.get('registro_dados')
-        # Converter a data de nascimento de volta para o tipo date
         dados_pessoais['data_nascimento'] = datetime.strptime(dados_pessoais['data_nascimento'], '%Y-%m-%d').date()
 
         form = FormularioInfoFinanceiras(request.POST)
         if form.is_valid():
-            usuario = Usuario(
-                primeiro_nome=dados_pessoais['primeiro_nome'],
-                ultimo_nome=dados_pessoais['ultimo_nome'],
-                nome_usuario=dados_pessoais['nome_usuario'],
-                cpf=dados_pessoais['cpf'],
-                telefone=dados_pessoais['telefone'],
-                data_nascimento=dados_pessoais['data_nascimento'],
-                email=dados_pessoais['email'],
-                senha=make_password(dados_pessoais['senha']),
-            )
-            usuario.save()
+            usuario = Usuario.objects.filter(email=dados_pessoais['email']).first()
+            if not usuario:
+                usuario = Usuario(
+                    primeiro_nome=dados_pessoais['primeiro_nome'],
+                    ultimo_nome=dados_pessoais['ultimo_nome'],
+                    nome_usuario=dados_pessoais['nome_usuario'],
+                    cpf=dados_pessoais['cpf'],
+                    telefone=dados_pessoais['telefone'],
+                    data_nascimento=dados_pessoais['data_nascimento'],
+                    email=dados_pessoais['email'],
+                    senha=make_password(dados_pessoais['senha']),
+                )
+                usuario.save()
+
             perfil_financeiro = form.save(usuario=usuario)
             usuario.tipo_perfil = perfil_financeiro.tipo_perfil
             usuario.save()
 
-            # Redirecionar e só depois remover os dados da sessão
-            if perfil_financeiro.tipo_perfil == 'Endividado':
-                return redirect('infoPerfilEndividado')
+            # Obtenha os valores do formulário como Decimal
+            renda = Decimal(form.cleaned_data.get('renda'))
+            divida = Decimal(form.cleaned_data.get('divida'))
+            patrimonio = Decimal(form.cleaned_data.get('patrimonio'))
+
+            # Lógica para decidir o perfil
+            if divida > patrimonio or renda < divida * Decimal(0.5):
+                return redirect('infoPerfilEndividado')  # Perfil Endividado
             else:
-                return redirect('infoPerfilInvestidor')
+                return redirect('infoPerfilInvestidor')  # Perfil Investidor
 
         return render(request, self.template_name, {'form': form})
