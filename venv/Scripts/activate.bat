3319be05a34964019b55cf5e21b55d7364248396
Alexandre Spinola <alexandre.cardoso1@estudante.ufla.br>
2025-02-02 17:01:47 -0300

    Minhas alterações antes de atualizar o projeto

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Alexandre\OneDrive\Área de Trabalho\Projeto Final\FinVest\venv"
+set "VIRTUAL_ENV=C:\Users\Alexandre\OneDrive\Área de Trabalho\ProjetoTestes\FinVest\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
